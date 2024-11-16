b21fd045eba076a9db5796fb7d0fb0b65bc9c9d3
FernandoC06 <80509138+FernandoC06@users.noreply.github.com>
2024-11-15 21:24:24 -0700

    Agregué modelos para checklist models.py
    Agregué los modelos checklist y task

--- mainPage/models.py
+++ mainPage/models.py
@@ -101,3 +101,22 @@ class Reportes(models.Model):
 
     def __str__(self):
         return self.title
+
+class Checklist(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='checklists', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    checklist = models.ForeignKey(Checklist, related_name='tasks', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    notes = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
